a313b00205865f50a0ec84ee18837ee7eb047b20
Shylie <fuzzcat927@gmail.com>
2021-08-12 19:24:38 -0400

    [demo] change cube color

--- demo/source/main.cpp
+++ demo/source/main.cpp
@@ -7,28 +7,28 @@
 void cube(glib& glib, float x, float y, float z, float sz)
 {
 	// front
-	glib.push({ x - sz / 2,  y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF });
-	glib.push({ x + sz / 2,  y + sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF });
+	glib.push({ x - sz / 2,  y - sz / 2, z + sz / 2, 0xFF0000FF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFF0000FF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFF0000FF });
+	glib.push({ x + sz / 2,  y + sz / 2, z + sz / 2, 0xFF0000FF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFF0000FF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFF0000FF });
 
 	// back
-	glib.push({ x - sz / 2,  y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF });
-	glib.push({ x + sz / 2,  y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFFFFFFFF });
+	glib.push({ x - sz / 2,  y - sz / 2, z - sz / 2, 0xFF0000FF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFF0000FF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0xFF0000FF });
+	glib.push({ x + sz / 2,  y + sz / 2, z - sz / 2, 0xFF0000FF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0xFF0000FF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFF0000FF });
 
 	// top
-	glib.push({ x - sz / 2,  y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF });
-	glib.push({ x + sz / 2,  y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF });
+	glib.push({ x - sz / 2,  y + sz / 2, z - sz / 2, 0x00FF00FF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0x00FF00FF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0x00FF00FF });
+	glib.push({ x + sz / 2,  y + sz / 2, z - sz / 2, 0x00FF00FF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0x00FF00FF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0x00FF00FF });
 
 	// bottom
-	glib.push({ x - sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF });
-	glib.push({ x + sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF });
+	glib.push({ x - sz / 2, y - sz / 2, z - sz / 2, 0x00FF00FF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0x00FF00FF }, { x - sz / 2, y - sz / 2, z + sz / 2, 0x00FF00FF });
+	glib.push({ x + sz / 2, y - sz / 2, z - sz / 2, 0x00FF00FF }, { x + sz / 2, y - sz / 2, z + sz / 2, 0x00FF00FF }, { x - sz / 2, y - sz / 2, z - sz / 2, 0x00FF00FF });
 
 	// right
-	glib.push({ x + sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y + sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF });
-	glib.push({ x + sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF });
+	glib.push({ x + sz / 2, y - sz / 2, z - sz / 2, 0x0000FFFF }, { x + sz / 2, y + sz / 2, z - sz / 2, 0x0000FFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0x0000FFFF });
+	glib.push({ x + sz / 2, y - sz / 2, z + sz / 2, 0x0000FFFF }, { x + sz / 2, y - sz / 2, z - sz / 2, 0x0000FFFF }, { x + sz / 2, y + sz / 2, z + sz / 2, 0x0000FFFF });
 
 	// left
-	glib.push({ x - sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0xFFFFFFFF });
-	glib.push({ x - sz / 2, y - sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0xFFFFFFFF }, { x - sz / 2, y - sz / 2, z - sz / 2, 0xFFFFFFFF });
+	glib.push({ x - sz / 2, y - sz / 2, z - sz / 2, 0x0000FFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0x0000FFFF }, { x - sz / 2, y + sz / 2, z - sz / 2, 0x0000FFFF });
+	glib.push({ x - sz / 2, y - sz / 2, z + sz / 2, 0x0000FFFF }, { x - sz / 2, y + sz / 2, z + sz / 2, 0x0000FFFF }, { x - sz / 2, y - sz / 2, z - sz / 2, 0x0000FFFF });
 }
 
 int main()
@@ -52,7 +52,7 @@ int main()
 			Mtx_Translate(glib.model(), 0.0f, 0.0f, -3.0f, true);
 			Mtx_Rotate(glib.model(), FVec3_New(0.1f, 0.1f, 0.1f), 5.0f * tm, true);
 
-			cube(glib, 0.0f, 0.0f, 0.0f, 1.0f + 0.5f * sinf(5.0f * tm));
+			cube(glib, 0.0f, 0.0f, 0.0f, 1.0f + 0.4f * sinf(5.0f * tm));
 
 			glib.endFrame();
 
